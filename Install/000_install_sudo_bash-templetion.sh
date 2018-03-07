60c13fc229257df55c159830b539b626c028f484
LimoYuan <yuan705791627@gmail.com>
2018-03-07 22:45:25 +0800

    New code .

--- Install/000_install_sudo_bash-templetion.sh
+++ Install/000_install_sudo_bash-templetion.sh
@@ -21,7 +21,52 @@ function installCache() {
   mkdir -p $workPath/.cache
   cd $workPath/.cache
 }
+function installDevice() {
+  #statements
+  case $1 in
+    vir )
+    apt install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,')
+    apt install xserver-xorg-input-evdev xserver-xorg-input-kbd xserver-xorg-input-mouse xserver-xorg-input-synaptics xserver-xorg-video-vesa xserver-xorg-video-vmware virtualbox-guest-x11 virtualbox-guest-utils
+    apt install apt install build-essential make perl
+    mount /dev/sr0 /mnt/ && cd /mnt
+    ./VBoxLinuxAdditions.run
+      ;;
+    * )
+    apt install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
+    apt install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') nvidia-driver
+    modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
+    modprobe wl
+    apt install xserver-xorg-input-evdev xserver-xorg-input-kbd xserver-xorg-input-mouse xserver-xorg-input-synaptics x11-xserver-utils x11-utils x11-xkb-utils
+      ;;
+  esac
+}
+function addUserToSudo() {
+  #statements
+  echo -en "\033[33m Please input you create username :  \033[0m"
+  read username
+  sed "20 a$username    ALL=(ALL:ALL) ALL" -i /etc/sudoers
+}
+function openBashCompletion() {
+  #statements
+cat <<BASH_COMPLETION | sudo tee /etc/bash.bashrc
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+BASH_COMPLETION
+}
 
+print_dot
+echo -e "\033[31m Install sudo , bash-completion , x11 . Add username to sudoers , open Shell Tab completion . \033[0m"
+print_dot
 # 安装 sudo , bash-completion . 开启 sudo 和 shell Tab 补全
 apt update
 apt install sudo bash-completion
+addUserToSudo
+openBashCompletion
+# 安装 X 环境 , 和一些驱动驱动
+installDevice
