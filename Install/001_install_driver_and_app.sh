ee006dac5dc0cee460d0210f947005c1455f31d6
LimoYuan <yuan705791627@gmail.com>
2018-03-09 04:55:51 +0800

    添加 xbindkeys 配置文件

--- Install/001_install_driver_and_app.sh
+++ Install/001_install_driver_and_app.sh
@@ -154,6 +154,8 @@ VIM_CONF
   cp -rf $workPath/stay.mp3 $HOME/Music
   # 移动 compton 配置文件到 ~/.config
   cp -rf $workPath/compton/compton.conf $HOME/.config/
+  # 添加 xbindkeys 配置文件
+  cp -rf $workPath/.xbindkeysrc $HOME
   # 对于一些GUI程序无法弹出需要 gksudo的窗口,这或许会有用
   sudo sed -i '4 asession required pam_loginuid.so' /etc/pam.d/lightdm
   sudo sed -i '5 asession required pam_systemd.so' /etc/pam.d/lightdm
